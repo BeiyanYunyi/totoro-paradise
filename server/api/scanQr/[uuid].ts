d8a7b07f1982ec74f33f7f8e6763b483bd715d15
BeiyanYunyi <14120445+BeiyanYunyi@users.noreply.github.com>
2023-02-09 13:01:35 +0800

    💩

--- server/api/scanQr/[uuid].ts
+++ server/api/scanQr/[uuid].ts
@@ -26,13 +26,14 @@ export default defineEventHandler(async (e) => {
         message: loginResult.message!,
       };
     }
-    // 获取额外信息
-    const personalInfo = await TotoroApiWrapper.login({ token: loginResult.token, code });
-    // 删除下面这行会导致 Vercel 504 Gateway Time-out，我不知道为什么。
-    console.log('personalInfo');
     return {
       message: '登录成功',
-      session: { ...personalInfo, token: loginResult.token, code, data: null },
+      session: {
+        ...(await TotoroApiWrapper.login({ token: loginResult.token, code })),
+        token: loginResult.token,
+        code,
+        data: null,
+      },
     };
   } catch (e) {
     console.log(e);
